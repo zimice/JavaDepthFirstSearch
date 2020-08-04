18af4e11d02a64c583cfe2ff39c01533c2f10689
zimice <simon.kochanek@me.com>
2020-08-04 19:51:20 +0200

    First commit

--- View.java
+++ View.java
@@ -28,9 +28,7 @@ public class View extends JFrame{
         setSize(640,480);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        DepthFirst.searchPath(maze,1,1,path);
-        System.out.println(path);
+        searchPath(maze,1,1,path);
     }
     @Override
     public void paint(Graphics g){
@@ -58,12 +56,61 @@ public class View extends JFrame{
     	   g.fillRect(pathX*30, pathY*30, 30, 30);
        }
     }
+    public static boolean searchPath(int[][] maze, int x, int y, List<Integer> path) {
+		
+		if ( maze[y][x] == 9) {
+			path.add(x);
+			path.add(y);
+			return true;
+		}
+		
+		if (maze [y][x] == 0) {
+			maze[y][x] =2;
+		
+		int dx = -1;
+		int dy = 0;
+		
+		if (searchPath(maze,x+dx,y+dy,path)) {
+			path.add(x);
+			path.add(y);
+			return true;
+		}
+		 dx = 1;
+		 dy = 0;
+		
+		if (searchPath(maze,x+dx,y+dy,path)) {
+			path.add(x);
+			path.add(y);
+			return true;
+		}
+		 dx = 0;
+		 dy = -1;
+		
+		if (searchPath(maze,x+dx,y+dy,path)) {
+			path.add(x);
+			path.add(y);
+			return true;
+		}
+		 dx = 0;
+		 dy = 1;
+		
+		if (searchPath(maze,x+dx,y+dy,path)) {
+			path.add(x);
+			path.add(y);
+			return true;
+		}
+
+		
+		}
+		return false;
+	}
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 View view = new View();
                 view.setVisible(true);
+                view.repaint();
             }
         });
     }
